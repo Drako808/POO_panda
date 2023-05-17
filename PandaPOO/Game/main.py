fd390024fa754a14386ab23cb83856c03b3cf2b9
Drako808 <drako8yes@gmail.com>
2023-05-16 22:52:46 -0500

    main 2

--- PandaPOO/Game/main.py
+++ PandaPOO/Game/main.py
@@ -5,8 +5,9 @@ pygame.init()
 screen = pygame.display.set_mode((470, 840))
 
 pygame.display.set_caption("Panda")
-icon = pygame.image.load("panda.png")
+icon = pygame.image.load("PandaPOO/Assets/panda.png")
 pygame.display.set_icon(icon)
+background_image = pygame.image.load("PandaPOO/Assets/fondo.png")
 
 running = True
 while running == True:
@@ -14,5 +15,5 @@ while running == True:
         if event.type == pygame.QUIT:
             running = False
             
-    screen.fill((0, 200, 0))
-    pygame.display.update()
+    screen.blit(background_image, (0, 0))  
+    pygame.display.flip()
